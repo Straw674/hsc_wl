ce0f07a3a37658d1eb8054bfc1540bbb11d0faf4
Xin Qiao <qx24@mails.tsinghua.edu.cn>
2026-04-19 09:15:15 +0800

    further initiation

---
+++ initial.py
@@ -0,0 +1,30 @@
+# avoid repeating import sentences
+# use `from imports import *` in your notebook
+# make sure this file is in the path of import
+# you may need to add the path using sys.path.append()
+
+import logging
+import math
+import os
+from copy import deepcopy
+from itertools import combinations
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.cosmology import Planck18
+from astropy.io import fits
+from astropy.table import Row, Table
+
+# matplotlib settings
+plt.rcParams["figure.dpi"] = 300
+desired_font = "Source Serif 4"
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = [desired_font] + plt.rcParams["font.serif"]
+
+# logging settings
+logging.basicConfig(level=logging.INFO)
+
+logging.info("Initialization complete.")
